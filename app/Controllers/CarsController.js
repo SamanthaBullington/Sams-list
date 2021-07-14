bc5f91549defc07cfc19e22aab3bb9983b79a2c3
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-07-14 16:43:04 -0600

    save

--- app/Controllers/CarsController.js
+++ app/Controllers/CarsController.js
@@ -1,34 +1,34 @@
 import { ProxyState } from "../AppState.js"
-import { houseService } from "../Services/HouseService.js"
+import { carsService } from "../Services/CarsService.js"
 
 function _draw() {
   let template = ''
   ProxyState.cars.forEach(car => {
     template += car.Template
   })
-  // document.getElementById('cars').innerHTML = template
+  document.getElementById('cars').innerHTML = template
 }
 
 export default class CarsController {
   constructor() {
 
-    ProxyState.on('house', _draw)
-    ProxyState.on('house', () => { console.log('new house') })
+    ProxyState.on('cars', _draw)
+    ProxyState.on('cars', () => { console.log('new car') })
     _draw()
   }
 
   makeCar() {
     event.preventDefault()
     let form = event.target
-    let newHouse = {
+    let newCar = {
+      make: form.make.value,
+      model: form.model.value,
       year: form.year.value,
-      bed: form.bed.value,
-      bath: form.bath.value,
       price: form.price.value,
       description: form.description.value,
       imgUrl: form.imgUrl.value
     }
-    houseService.makeHouse(newHouse)
+    carsService.makeCar(newCar)
     form.reset()
   }
 }
